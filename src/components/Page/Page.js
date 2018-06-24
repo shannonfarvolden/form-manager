d7f6e714cdc6355551754343561fe96e396c4a9b
Plinio Prado <plinioprado01@gmail.com>
2018-06-23 23:30:14 -0700

    page with link and other improvements

--- src/components/Page/Page.js
+++ src/components/Page/Page.js
@@ -1,13 +1,22 @@
 import React from "react";
 
 import ConfigDialog from '../ConfigDialog'
+import EditIcon from '@material-ui/icons/Edit';
+import Tooltip from '@material-ui/core/Tooltip';
 
-const Page = ({ pageConfig, selectedFieldId, dialogOpen, dialogCancel, dialogConfirm }) => {
+const Page = ({ formHeaderConfig, copyHeaderConfig, pageConfig, selectedFieldId, dialogOpen, dialogCancel, dialogConfirm }) => {
 
   const newField = {...pageConfig}
-
   const handleChange = (e) => {
     console.log('handling locally', e)
+  }
+
+  const pageHeaderStyle = {
+    position: 'absolute',
+    marginTop: '100px',
+    top: 0,
+    left: 0,
+    width: pageConfig.width || copyHeaderConfig.width || formHeaderConfig.width || '100%'
   }
 
   return (
@@ -16,15 +25,9 @@ const Page = ({ pageConfig, selectedFieldId, dialogOpen, dialogCancel, dialogCon
     :
     (<div>
       <img
-        src={require('./../../images/ex_w8_1.png')}
+        src={require('./../../images/' + ( pageConfig.bgImage || copyHeaderConfig.bgImage || formHeaderConfig.bgImage || 'blank.png' ))}
         alt=''
-        style={{
-          position: 'absolute',
-          marginTop: '100px',
-          top: 0,
-          left: 0,
-          width: '800px'
-        }}
+        style={pageHeaderStyle}
       />
       <div
         style={{
@@ -38,27 +41,48 @@ const Page = ({ pageConfig, selectedFieldId, dialogOpen, dialogCancel, dialogCon
       {Object.keys(pageConfig).map((fieldId, index) => {
         const field = pageConfig[fieldId];
         return (
-          <input
-              type='text'
-              name={fieldId}
-              id={`${'ex_w8'}-${'1'}}-${'1'}-${fieldId}`}
-              key={index}
-              value={field.defaultValue}
-              onChange={e => this.handleChange(e)}
-              onClick={() => dialogOpen(fieldId)}
-              style={{
-                position: 'absolute',
-                top: field.top,
-                left: field.left,
-                width: field.width,
-                height: field.height,
-                backgroundColor: 'beige'
-              }}
-            />)
+          <div
+            key={index}
+            style={{
+              position: 'absolute',
+              top: field.top,
+              left: field.left
+            }}
+          >
+            <Tooltip id="tooltip-top-end" title="Click to edit field configs" placement="top-end">
+              <EditIcon
+                style={{
+                  float:'right',
+                  cursor: 'pointer',
+                  width: field.height,
+                  height: field.height,
+                  backgroundColor: '#ADD8E6',
+                  border: '1px solid #000',
+                  padding: '2px'
+                }}
+                onClick={() => dialogOpen(fieldId)}
+              />
+            </Tooltip>
+            <Tooltip id="tooltip-top-end" title="Simulated field with default" placement="top-end">     
+              <input
+                  type='text'
+                  name={fieldId}
+                  id={`${'ex_w8'}-${'1'}}-${'1'}-${fieldId}`}
+                  key={index}
+                  value={field.defaultValue}
+                  style={{
+                    width: field.width,
+                    height: field.height,
+                    backgroundColor: '#ADD8E6',
+                    border: '2px solid transparent',
+                    padding: '1px'   
+                  }}
+                />
+              </Tooltip>
+            </div>)
       })
       }
       </div>
-      selectedFieldId &&
       <ConfigDialog
           fieldId={selectedFieldId}
           field={pageConfig[selectedFieldId]}
