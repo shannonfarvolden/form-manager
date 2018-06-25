af6b0bd89fb2de4213d2658493305d00fad54868
Shannon <shannonfarvolden@hotmail.com>
2018-06-25 00:53:21 -0700

    add node server and google oauth

---
+++ index.js
@@ -0,0 +1,7 @@
+const express = require("express");
+require("./services/passport");
+const authRoutes = require("./routes/authRoutes.js");
+const app = express();
+authRoutes(app);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
