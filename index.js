1aa1d1d44d1d93a0001a0ef379b22decca0ccd60
Shannon <shannonfarvolden@hotmail.com>
2018-06-26 00:11:51 -0700

    add mongodb model and cookie session

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
 const express = require("express");
-require("./services/passport");
+const mongoose = require("mongoose");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
 const authRoutes = require("./routes/authRoutes.js");
+const keys = require("./config/keys");
+require("./models/Users");
+require("./services/passport");
+
+mongoose.connect(keys.mongoURI);
+
 const app = express();
+
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
 authRoutes(app);
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
