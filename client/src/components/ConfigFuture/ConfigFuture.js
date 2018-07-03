dc8f2b224d874b5825746b5b6fd5a74ec1019d6e
Plinio Prado <plinioprado01@gmail.com>
2018-07-02 23:50:40 -0700

    changing configs

---
+++ client/src/components/ConfigFuture/ConfigFuture.js
@@ -0,0 +1,44 @@
+import React from "react";
+
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import TextField from '@material-ui/core/TextField'
+
+const ConfigFuture = ({newField, handleTexboxChange}) => {
+
+  console.log('newField=', newField)
+
+  return (
+    <List>
+        <ListItem key='1'>
+          <TextField
+            label="populate"
+            name="populate"
+            value={newField.populate}
+            onChange={e => handleTexboxChange(e)}
+            disabled={true}
+          />
+        </ListItem>
+        <ListItem key='2'>
+          <TextField
+            label="mask"
+            name="mask"
+            value={newField.mask}
+            onChange={e => handleTexboxChange(e)}
+            disabled={true}
+          />
+        </ListItem>
+        <ListItem key='3'>
+          <TextField
+            label="newForm"
+            name="newForm"
+            value={newField.newForm}
+            onChange={e => handleTexboxChange(e)}
+            disabled={true}
+          />
+        </ListItem>
+    </List>
+  )
+}
+
+export default ConfigFuture;
