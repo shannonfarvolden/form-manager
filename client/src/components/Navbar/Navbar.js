8d64fee29e72f9e574e140c0373012289056ee84
Shannon <shannonfarvolden@hotmail.com>
2018-07-04 01:45:02 -0700

    add auth reducer, google oauth to client

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { withStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
@@ -6,6 +6,8 @@ import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+
 import logo from "../../images/logo.png";
 
 const styles = {
@@ -28,37 +30,52 @@ const styles = {
   }
 };
 
-const Navbar = props => {
-  const { classes } = props;
-  return (
-    <div className={classes.root}>
-      <AppBar position="static" color="default">
-        <Toolbar>
-          <Link to="/">
-            <img src={logo} className={classes.logoIcon} alt="logo" />
-          </Link>
+class Navbar extends Component {
+  renderContent() {
+    switch (this.props.auth) {
+      case null:
+        return;
+      case false:
+        return <a href="/auth/google">Login With Google</a>;
+      default:
+        return [<a href="/api/logout">Logout</a>];
+    }
+  }
+  render() {
+    const { classes } = this.props;
 
-          <Link className={classes.link} to="/">
-            <Button className={classes.menuButton} color="primary">
-              <Typography>Home</Typography>
-            </Button>
-          </Link>
+    return (
+      <div className={classes.root}>
+        <AppBar position="static" color="default">
+          <Toolbar>
+            <Link to="/">
+              <img src={logo} className={classes.logoIcon} alt="logo" />
+            </Link>
 
-          <Link className={classes.link} to="/form">
-            <Button className={classes.menuButton} color="primary">
-              <Typography>Forms</Typography>
-            </Button>
-          </Link>
+            <Link className={classes.link} to="/">
+              <Button className={classes.menuButton} color="primary">
+                <Typography>Home</Typography>
+              </Button>
+            </Link>
 
-
-        </Toolbar>
-      </AppBar>
-    </div>
-  );
-};
+            <Link className={classes.link} to="/form">
+              <Button className={classes.menuButton} color="primary">
+                <Typography>Forms</Typography>
+              </Button>
+            </Link>
+            {this.renderContent()}
+          </Toolbar>
+        </AppBar>
+      </div>
+    );
+  }
+}
 
 Navbar.propTypes = {
   classes: PropTypes.object.isRequired
 };
 
-export default withStyles(styles)(Navbar);
+function mapStateToProps({ auth }) {
+  return { auth };
+}
+export default withStyles(styles)(connect(mapStateToProps)(Navbar));
--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
 import "./index.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
+import store from "./redux/store";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+// Development only axios helpers!
+import axios from "axios";
+window.axios = axios;
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 registerServiceWorker();
