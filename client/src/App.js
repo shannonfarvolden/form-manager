f85532dc7de7b76a16f47de0b78c6400a446e4fd
Shannon <shannonfarvolden@hotmail.com>
2018-07-04 01:45:02 -0700

    add auth reducer, google oauth to client

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,28 @@
 import React, { Component } from "react";
-import { Provider } from "react-redux";
 import { BrowserRouter as Router } from "react-router-dom";
+import { connect } from "react-redux";
 
 import Navbar from "./components/Navbar";
 import Main from "./components/Main";
+import * as actions from "./redux/actions";
 
 import "./App.css";
 
-import store from "./redux/store";
-
 class App extends Component {
+  componentDidMount() {
+    this.props.fetchUser();
+  }
+
   render() {
     return (
-      <Provider store={store}>
-        <Router>
-          <div className="App">
-            <Navbar />
-            <Main />
-          </div>
-        </Router>
-      </Provider>
+      <Router>
+        <div className="App">
+          <Navbar />
+          <Main />
+        </div>
+      </Router>
     );
   }
 }
 
-export default App;
+export default connect(null, actions)(App);
--- client/src/redux/reducers.js
+++ client/src/redux/reducers.js
@@ -1,8 +1,9 @@
-import { combineReducers } from 'redux'
+import { combineReducers } from "redux";
 
-import { configReducer } from './modules/formConfig'
-
+import { configReducer } from "./modules/formConfig";
+import authReducer from "./modules/authReducer";
 export default combineReducers({
-  formConfig: configReducer
+  formConfig: configReducer,
   // Later will add at least router
-})
+  auth: authReducer
+});
