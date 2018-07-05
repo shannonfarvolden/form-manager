f5f9ad3293a7665347afd8479119a7ed768761eb
Alex Tang <alextang3@outlook.com>
2018-07-04 20:44:07 -0700

    got error showing on sidebar

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -23,6 +23,15 @@ const Form = ({ formHeaderConfig, copyHeaderConfig, pageConfig, selectedFieldId,
 
       {Object.keys(pageConfig).map((fieldId, index) => {
         const field = pageConfig[fieldId];
+        let isValid = true;
+        if (field.valid) {
+          const body = `return ${field.valid}`;
+          const fn = new Function('val', body);
+          const fnVal = fn(field.value);
+          if (fnVal === true || fnVal === false) isValid = fnVal;
+        }
+        let invalid = ((field.mandatory && field.value === '' ) || !isValid);
+
         return (
           <div
             key={index}
@@ -55,10 +64,11 @@ const Form = ({ formHeaderConfig, copyHeaderConfig, pageConfig, selectedFieldId,
                   key={index}
                   value={field.value}
                   onChange={handleChangeValue}
+                  disabled={field.disabled}
                   style={{
                     width: field.width,
                     height: field.height,
-                    backgroundColor: '#ADD8E6',
+                    backgroundColor: field.disabled ? '	#D3D3D3' : (invalid ? '#FFFF00' : '#ADD8E6'),
                     border: '2px solid transparent',
                     padding: '1px'
                   }}
